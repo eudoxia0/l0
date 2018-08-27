714d1a6a0c802ac58a6361fe578955a549adb346
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-27 20:33:17 -0300

    test tuple bind

--- examples/tuple.lisp
+++ examples/tuple.lisp
@@ -7,4 +7,12 @@
     (println (proj tup 0))
     (print "(proj tup 1) = ")
     (println (proj tup 1)))
+  (let ((tup (tuple 1 2 3)))
+    (bind (a b c) tup
+      (print "a = ")
+      (println a)
+      (print "b = ")
+      (println b)
+      (print "c = ")
+      (println c)))
   0)
