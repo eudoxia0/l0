2c4bc29d50ac1aef48f24ea7a659d154bdaf7dee
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-27 20:31:54 -0300

    Add tuple.lisp

---
+++ examples/tuple.lisp
@@ -0,0 +1,10 @@
+;; -*- mode: lisp -*-
+
+(defun main () i32
+  (let ((tup (tuple 3 4)))
+    (println "Hello, world!")
+    (print "(proj tup 0) = ")
+    (println (proj tup 0))
+    (print "(proj tup 1) = ")
+    (println (proj tup 1)))
+  0)
