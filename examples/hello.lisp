ef5737b2c84c9dcfd856966b290420c73ed1cc1b
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-27 20:59:21 -0300

    Fix

--- examples/hello.lisp
+++ examples/hello.lisp
@@ -1,13 +1,5 @@
 ;; -*- mode: lisp -*-
 
 (defun main () i32
-  (let ((a 10))
-    (println a))
-  (let ((a 10))
-    (println a))
-  (let ((a 10))
-    (println a))
-  (let ((a 10))
-    (println a))
   (println "Hello, world!")
   0)
